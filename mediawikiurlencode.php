3f8e964df32e362ad8e9b16d18559de3eb2bf710
xiplus <huangxuanyuxiplus@gmail.com>
2018-02-13 21:45:27 +0800

    " to %22

--- mediawikiurlencode.php
+++ mediawikiurlencode.php
@@ -1,7 +1,18 @@
 <?php
 
 function mediawikiurlencode($baseurl, $page, $section="") {
-	$url = $baseurl.str_replace([" ", "“", "”", "\xE2\x80\x8E"], ["_", "%E2%80%9C", "%E2%80%9D", ""], $page);
+	$replacement = [
+		" " => "_",
+		"“" => "%E2%80%9C",
+		"”" => "%E2%80%9D",
+		"\xE2\x80\x8E" => "",
+		"‧" => "%E2%80%A7",
+		"\"" => "%22"
+	];
+	foreach ($replacement as $from => $to) {
+		$page = str_replace($from, $to, $page);
+	}
+	$url = $baseurl.$page;
 	$section = trim($section);
 	if ($section !== "") {
 		$url .= '#'.str_replace([" ", "“", "”", "\xE2\x80\x8E"], ["_", "%E2%80%9C", "%E2%80%9D", ""], $section);
